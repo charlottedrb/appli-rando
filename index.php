504158378be8046988609f21cde6e664901457fa
Charlotte DER BAGHDASSARIAN <Ch.DERBAGHDASSARIAN@formation-cci.fr>
2020-09-28 10:20:06 +0200

    init includes

--- index.php
+++ index.php
@@ -4,6 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Appli rando</title>
+    <?php
+    include 'style.php';
+    include 'scripts.php';
+
+    ?>
 </head>
 <body>
     
